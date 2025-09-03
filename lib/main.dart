58b468c90f16689fe756e726b64fbe1fbcea625f
Atiq Hasan Sani <m1@Atiqs-MacBook-Pro.local>
2025-09-04 03:47:34 +0600

    add splash screen and jump to next page, gitignore added

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager/app.dart';
+
+void main(){
+  // Try to write less code in main
+  runApp(TaskManagerApp());
+}
---
+++ lib/ui/utils/assets_path.dart
@@ -0,0 +1,6 @@
+class AssetsPath{
+  static const String _imagePath = "assets/images";
+
+  static const String backgroundImage = "$_imagePath/background.svg";
+  static const String logoImage = "$_imagePath/logo.svg";
+}
